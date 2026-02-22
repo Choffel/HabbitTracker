b0d28ab1a52ba58f2de6989ddb9c4755dcc7362b
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-22 21:50:50 +0100

    chore(di): register application services and repositories

--- HabbitApi/Program.cs
+++ HabbitApi/Program.cs
@@ -1,9 +1,22 @@
+using HabbitApi.Interface;
+using HabbitApi.Mapper;
+using HabbitApi.Repository;
+using HabbitApi.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(cfg => { }, typeof(HabitMappingProfile));
+
+//services
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IHabitRepository,HabitRepository>();
+builder.Services.AddScoped<IHabitService, HabitService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
