a5c1447658ac667f00f0d9985f7e1e72b0b858b4
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-22 21:49:52 +0100

    feat(mapping): add AutoMapper profile and dependencies

---
+++ HabbitApi/Mapper/HabitMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HabbitApi.DTOs;
+using HabbitApi.Interface;
+using HabbitApi.Model;
+
+namespace HabbitApi.Mapper;
+
+public class HabitMappingProfile : Profile
+{
+    public HabitMappingProfile()
+    {
+        CreateMap<Habit, HabitResponseDTO>();
+        
+        CreateMap<AddHabitRequestDTO, Habit>();
+        
+        CreateMap<UpdateHabitRequest, Habit>();
+    }
+}
