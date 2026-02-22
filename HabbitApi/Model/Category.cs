971a631cf601171664cced129f6ab1935b4fe2bf
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-22 23:23:04 +0100

    refactor(model): make Habits collection public in Category

--- HabbitApi/Model/Category.cs
+++ HabbitApi/Model/Category.cs
@@ -1,10 +1,12 @@
-﻿namespace HabbitApi.Model;
+﻿using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Expressions.Internal;
+
+namespace HabbitApi.Model;
 
 public class Category
 {
     public Guid Id { get; set; }
 
     public string Name { get; set; }
-    
-    ICollection<Habit> Habits { get; set; } = new List<Habit>();
+
+    public ICollection<Habit> Habits { get; set; } = new List<Habit>();
 }
