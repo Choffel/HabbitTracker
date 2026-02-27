e583011bd5d146932ac9be0bfb21e692d9361174
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-27 23:33:02 +0100

    feat(DbContext): add new colum

--- HabbitApi/Data/ApplicationDbContext.cs
+++ HabbitApi/Data/ApplicationDbContext.cs
@@ -14,6 +14,8 @@ public class ApplicationDbContext : DbContext
     
     public DbSet<Category> Categories { get; set; }
     
+    public DbSet<HabitLog> habitLogs { get; set; }
+    
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
