5b08bbdb36ed62ace73830427c1aeb2f8c443aa0
Daniil Choffel <daniilmiadzve@gmail.com>
2026-02-27 23:34:43 +0100

    feat(HabitLog):add contract and implementation HabitLogRepository

---
+++ HabbitApi/Interface/IHabitLogRepository.cs
@@ -0,0 +1,12 @@
+﻿using HabbitApi.Model;
+
+namespace HabbitApi.Interface;
+
+public interface IHabitLogRepository
+{
+    Task<HabitLog?> GetByIdAsync(Guid id);
+    
+    Task<IEnumerable<HabitLog>> GetByHabitIdAsync(Guid habitId);
+    
+    Task AddAsync(HabitLog habitLog);
+}
---
+++ HabbitApi/Repository/HabitLogRepository.cs
@@ -0,0 +1,28 @@
+﻿using HabbitApi.Data;
+using HabbitApi.Interface;
+using HabbitApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabbitApi.Repository;
+
+public class HabitLogRepository : IHabitLogRepository
+{
+    private readonly ApplicationDbContext _context;
+    
+    public async Task<HabitLog?> GetByIdAsync(Guid id)
+    {
+        return await _context.habitLogs.FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async  Task<IEnumerable<HabitLog>> GetByHabitIdAsync(Guid habitId)
+    {
+        return await _context.habitLogs.Where(a => a.HabitId == habitId)
+            .OrderBy(a => a.LogDate)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(HabitLog habitLog)
+    {
+        await _context.habitLogs.AddAsync(habitLog);
+    }
+}
